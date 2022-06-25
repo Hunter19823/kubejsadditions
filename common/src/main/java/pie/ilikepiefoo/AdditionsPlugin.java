50f1cdcab9e7a48c9ca3c78bab82672fefc5555c
Hunter19823 <hspragg+github@asu.edu>
2022-06-25 16:13:39 -0700

    Version 2.0.2: - Added global binding DamageSource for making custom damage sources. (DamageSource.custom(String id))

--- common/src/main/java/pie/ilikepiefoo/AdditionsPlugin.java
+++ common/src/main/java/pie/ilikepiefoo/AdditionsPlugin.java
@@ -2,14 +2,21 @@ package pie.ilikepiefoo;
 
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
 import dev.latvian.mods.kubejs.script.ScriptType;
 import dev.latvian.mods.rhino.util.wrap.TypeWrappers;
 import net.minecraft.world.level.ChunkPos;
+import pie.ilikepiefoo.player.CustomDamageSourceJS;
 import pie.ilikepiefoo.util.SectionUtils;
 
 public class AdditionsPlugin extends KubeJSPlugin {
 	@Override
 	public void addTypeWrappers(ScriptType type, TypeWrappers typeWrappers) {
 		typeWrappers.register(ChunkPos.class, SectionUtils::getChunkPos);
+	}
+
+	@Override
+	public void addBindings(BindingsEvent event) {
+		event.add("DamageSource", CustomDamageSourceJS.class);
 	}
 }
