b2c5250adbe6802d39785ea70a0fec156fbd6cdd
Hunter19823 <hspragg@asu.edu>
2023-05-06 19:06:49 -0700

    Fixed an oversight on JEIEvents

--- common/src/main/java/pie/ilikepiefoo/compat/jei/JEIEvents.java
+++ common/src/main/java/pie/ilikepiefoo/compat/jei/JEIEvents.java
@@ -5,6 +5,14 @@ import dev.latvian.mods.kubejs.event.EventHandler;
 import pie.ilikepiefoo.compat.jei.events.OnRuntimeAvailableEventJS;
 import pie.ilikepiefoo.compat.jei.events.RegisterAdvancedEventJS;
 import pie.ilikepiefoo.compat.jei.events.RegisterCategoriesEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterFluidSubtypeEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterGUIHandlersEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterIngredientsEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterItemSubtypeEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterRecipeCatalystsEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterRecipeTransferHandlersEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterRecipesEventJS;
+import pie.ilikepiefoo.compat.jei.events.RegisterVanillaCategoryExtensionsEventJS;
 
 public interface JEIEvents {
 	EventGroup GROUP = EventGroup.of("JEIAddedEvents");
@@ -12,14 +20,14 @@ public interface JEIEvents {
 	EventHandler ON_RUNTIME_AVAILABLE = GROUP.client("onRuntimeAvailable", () -> OnRuntimeAvailableEventJS.class);
 	EventHandler REGISTER_ADVANCED = GROUP.client("registerAdvanced", () -> RegisterAdvancedEventJS.class);
 	EventHandler REGISTER_CATEGORIES = GROUP.client("registerCategories", () -> RegisterCategoriesEventJS.class);
-	EventHandler REGISTER_FLUID_SUBTYPES = GROUP.client("registerFluidSubtypes", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_GUI_HANDLERS = GROUP.client("registerGUIHandlers", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_INGREDIENTS = GROUP.client("registerIngredients", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_ITEM_SUBTYPES = GROUP.client("registerItemSubtypes", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_RECIPE_CATALYSTS = GROUP.client("registerRecipeCatalysts", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_RECIPES = GROUP.client("registerRecipes", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_RECIPE_TRANSFER_HANDLERS = GROUP.client("registerRecipeTransferHandlers", () -> RegisterAdvancedEventJS.class);
-	EventHandler REGISTER_VANILLA_CATEGORY_EXTENSIONS = GROUP.client("registerVanillaCategoryExtensions", () -> RegisterAdvancedEventJS.class);
+	EventHandler REGISTER_FLUID_SUBTYPES = GROUP.client("registerFluidSubtypes", () -> RegisterFluidSubtypeEventJS.class);
+	EventHandler REGISTER_GUI_HANDLERS = GROUP.client("registerGUIHandlers", () -> RegisterGUIHandlersEventJS.class);
+	EventHandler REGISTER_INGREDIENTS = GROUP.client("registerIngredients", () -> RegisterIngredientsEventJS.class);
+	EventHandler REGISTER_ITEM_SUBTYPES = GROUP.client("registerItemSubtypes", () -> RegisterItemSubtypeEventJS.class);
+	EventHandler REGISTER_RECIPE_CATALYSTS = GROUP.client("registerRecipeCatalysts", () -> RegisterRecipeCatalystsEventJS.class);
+	EventHandler REGISTER_RECIPES = GROUP.client("registerRecipes", () -> RegisterRecipesEventJS.class);
+	EventHandler REGISTER_RECIPE_TRANSFER_HANDLERS = GROUP.client("registerRecipeTransferHandlers", () -> RegisterRecipeTransferHandlersEventJS.class);
+	EventHandler REGISTER_VANILLA_CATEGORY_EXTENSIONS = GROUP.client("registerVanillaCategoryExtensions", () -> RegisterVanillaCategoryExtensionsEventJS.class);
 
 	static void register() {
 		GROUP.register();
